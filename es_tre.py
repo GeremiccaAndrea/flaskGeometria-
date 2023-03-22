1b7d63a21808ddf715c1ffe489613b941044570f
Andrea Geremicca <geremicca.andrea@iisgalvanimi.edu.it>
2023-03-22 13:44:53 +0000

    es 3

---
+++ es_tre.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template
+import math
+app = Flask(__name__)
+
+
+@app.route('/')
+def es_tre():
+    return render_template('es_tre.html')
+
+
+@app.route('/data', methods = ['GET'])
+def geo():
+    geometria = float(request.args.get("geometria"))
+    base = float(request.args.get('base'))
+    altezza = float(request.args.get('altezza'))
+    if calcolo == "Area":
+        area = base * altezza
+        return render_template('es3area.html',Base = base,Altezza=altezza, Area = area)
+    else:
+        diagonale = math.sqrt(base ** 2 + altezza ** 2)
+        return render_template('es3diagonale.html',Base = base,Altezza=altezza, Diagonale = diagonale)
+
+
+
+
+@app.route('/es_cinque', methods = ["GET"])
+def es_cinque2():
+    forme = request.args.get("forme") 
+    if forme == "cerchio":      
+        return render_template("cerchio.html")
+    elif forme == "rettangolo":
+        return render_template("rettangolo.html")
+    elif forme == "pentagono":
+        return render_template("pentagono.html")
+    else: 
+        return render_template("quadrato.html")
+
+
+@app.route('/areaquadrato' , methods = ['GET'])
+def areaquadrato():
+  lato = int(request.args.get('lato')) 
+  area = lato * lato 
+  return render_template("risultato_area_quadrato_es1.html", area = area)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3246, debug=True)
